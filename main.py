ec7fc56dc834519ef1772f40164af517a27ada77
Jeffrey <test@example.com>
2021-09-03 17:42:10 -0500

    Made bot into an Object which uses a Cog for the commands

--- main.py
+++ main.py
@@ -2,26 +2,46 @@ import logging
 import os
 
 import discord
+from discord.ext import commands
 
 logger = logging.getLogger(__file__)
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+class MyClient(commands.Bot):
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    def __init__(self, command_prefix) -> None:
+        commands.Bot.__init__(self, command_prefix=command_prefix, self_bot=False)
+        self.message1 = "[INFO]: Bot now online"
+    
+    async def on_ready(self):
+        print(self.message1)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+
+class Greetings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+    
+    @commands.command()
+    async def hello(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send('Hello {0.name}~'.format(member))
+        else:
+            await ctx.send('Hello {0.name}... This feels familiar.'.format(member))
+        self._last_member = member
+
 
 if __name__ == "__main__":
     logging.info("Starting bot!")
+
     token = os.getenv('TOKEN')
     if not token:
         logging.warning("Did not find a token in the environment for the bot!")
-    client.run(token)
+
+    BOT_COMMAND_PREFIX = "!"
+
+    bot = MyClient(command_prefix=BOT_COMMAND_PREFIX)
+    bot.add_cog(Greetings(bot))
+    bot.run(token)
