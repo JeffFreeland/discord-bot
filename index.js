883dacd131b738c12a2635459ef4eb841368ac7f
Jeffrey <test@example.com>
2021-09-23 20:48:18 -0500

    moved commands to folder; only kept ping

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const fs = require("fs");
+const { Client, Collection, Intents } = require('discord.js');
 const log4js = require("log4js");
 
 var logger = log4js.getLogger();
@@ -8,8 +8,14 @@ logger.level = "info";
 function main() {
     // Create a new client instance
     const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+    client.commands = new Collection();
 
-    // When the client is ready, run this code (only once)
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.data.name, command);
+    }
+
     client.once('ready', () => {
         logger.info('Bot is ready!');
     });
@@ -17,15 +23,17 @@ function main() {
     client.on('interactionCreate', async interaction => {
         if (!interaction.isCommand()) return;
 
-        const { commandName } = interaction;
+        const command = client.commands.get(interaction.commandName);
 
-        if (commandName === 'ping') {
-            await interaction.reply('Pong!');
-        } else if (commandName === 'server') {
-            await interaction.reply(`Server Name: ${interaction.guild.name}\nTotal Server Members: ${interaction.guild.memberCount}`);
-        } else if (commandName === 'user') {
-            await interaction.reply('User info.');
+        if(!command) return;
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            logger.error(error)
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
+        
     });
 
     const TOKEN = process.env.TOKEN;
@@ -36,3 +44,15 @@ function main() {
 }
 
 main();
+
+
+
+/*
+        if (commandName === 'ping') {
+            await interaction.reply('Pong!');
+        } else if (commandName === 'server') {
+            await interaction.reply(`Server Name: ${interaction.guild.name}\nTotal Server Members: ${interaction.guild.memberCount}`);
+        } else if (commandName === 'user') {
+            await interaction.reply('User info.');
+        }
+*/
