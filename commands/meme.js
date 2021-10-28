17a5aa242f9b0a3091d6e3c1368375b120902dac
Jeffrey <test@example.com>
2021-10-27 19:10:01 -0500

    added image urls to a config folder

--- commands/meme.js
+++ commands/meme.js
@@ -1,46 +1,31 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Random } = require("random-js");
-
-const IMAGE_URLS = [
-    'https://i.imgur.com/YE6230n.jpg',
-    'https://i.imgur.com/Hprww8w.png',
-    'https://i.imgur.com/kfmlKd3.png',
-    'https://i.imgur.com/nfvEKOb.jpg',
-    'https://i.imgur.com/BkMfLRf.jpg',
-    'https://i.imgur.com/sSAFFtt.png',
-    'https://i.imgur.com/9rjgZY0.jpg',
-    'https://i.imgur.com/Lx26LzO.jpg',
-    'https://i.imgur.com/IJS6qwk.jpg',
-    'https://i.imgur.com/r1f336f.jpg',
-    'https://i.imgur.com/FsMkMcQ.jpg',
-    'https://i.imgur.com/WGtcEGy.jpg',
-    'https://i.imgur.com/vCeBrsi.jpg',
-    'https://i.imgur.com/5ZsKXF0.jpg',
-    'https://i.imgur.com/2WghjMO.jpg',
-    'https://i.imgur.com/Ihr8YjP.jpg',
-    'https://i.imgur.com/5aui968.jpg',
-    'https://i.imgur.com/B8ZUuTF.jpg',
-    'https://i.imgur.com/GEQx6UH.jpg',
-    'https://i.imgur.com/oypvCOm.jpg',
-    'https://i.imgur.com/mkc0rMA.jpg',
-    'https://i.imgur.com/0N2hP3u.jpg',
-    'https://i.imgur.com/OD2C4zi.jpg',
-    'https://i.imgur.com/AqFrOOu.jpg',
-    'https://i.imgur.com/rozt5IV.jpg',
-    'https://i.imgur.com/V3pMcwk.jpg',
-    'https://i.imgur.com/N4HRQD1.jpg',
-    'https://i.imgur.com/Ct7MRVU.jpg',
-    'https://i.imgur.com/lvGuwex.jpg'
-];
+const logger = require("../logging.js");
+const fs = require("fs");
 
 module.exports = {
     data: new SlashCommandBuilder()
             .setName('meme')
             .setDescription('A leftist meme for the working class'),
     async execute(interaction) {
+        const images = getImages();
+
         const random = new Random();
-        const image_index = random.integer(0, IMAGE_URLS.length-1);
-        const image = IMAGE_URLS[image_index];
+        const image_index = random.integer(0, images.length-1);
+        const image = images[image_index];
         await interaction.reply(image);
     } 
+}
+
+function getImages() {
+    let imageFile = process.env.IMAGES_FILE;
+    if(!imageFile){
+        const DEFAULT_IMAGES_FILE = 'config/image-urls.txt';
+        logger.debug(`IMAGES_FILE not specified, using default: ${DEFAULT_IMAGES_FILE}`);
+        imageFile = DEFAULT_IMAGES_FILE;
+    }
+
+    let images = [];
+    images = fs.readFileSync(imageFile, 'utf-8').split('\r\n');
+    return images;
 }
